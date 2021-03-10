8d4e690b2caa2f797939c3e5fa6f4739cf5b90ae
wingsmight <wingsmight@gmail.com>
2021-03-10 20:19:14 +0700

    add sign-in form

---
+++ SystemAnalysis1/SignInForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SystemAnalysis1
+{
+    partial class SignInForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignInForm));
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.SignInAsLabel = new System.Windows.Forms.Label();
+            this.SingExpertButton = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.SignAnalystButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(0, 0);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(800, 57);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Приложение для работы с оцениванием проблемы";
+            this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SignInAsLabel
+            // 
+            this.SignInAsLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SignInAsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignInAsLabel.Location = new System.Drawing.Point(0, 57);
+            this.SignInAsLabel.Name = "SignInAsLabel";
+            this.SignInAsLabel.Size = new System.Drawing.Size(800, 116);
+            this.SignInAsLabel.TabIndex = 1;
+            this.SignInAsLabel.Text = "Войти как:";
+            this.SignInAsLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // SingExpertButton
+            // 
+            this.SingExpertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SingExpertButton.ForeColor = System.Drawing.Color.Brown;
+            this.SingExpertButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.SingExpertButton.ImageIndex = 1;
+            this.SingExpertButton.ImageList = this.imageList;
+            this.SingExpertButton.Location = new System.Drawing.Point(180, 187);
+            this.SingExpertButton.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
+            this.SingExpertButton.Name = "SingExpertButton";
+            this.SingExpertButton.Padding = new System.Windows.Forms.Padding(0, 0, 0, 15);
+            this.SingExpertButton.Size = new System.Drawing.Size(154, 156);
+            this.SingExpertButton.TabIndex = 2;
+            this.SingExpertButton.Text = "Эксперт";
+            this.SingExpertButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.SingExpertButton.UseVisualStyleBackColor = true;
+            this.SingExpertButton.Click += new System.EventHandler(this.SingExpertButton_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "ProfilePicture-01.png");
+            this.imageList.Images.SetKeyName(1, "ProfilePicture-01.png");
+            // 
+            // SignAnalystButton
+            // 
+            this.SignAnalystButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignAnalystButton.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.SignAnalystButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.SignAnalystButton.ImageIndex = 1;
+            this.SignAnalystButton.ImageList = this.imageList;
+            this.SignAnalystButton.Location = new System.Drawing.Point(450, 187);
+            this.SignAnalystButton.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
+            this.SignAnalystButton.Name = "SignAnalystButton";
+            this.SignAnalystButton.Padding = new System.Windows.Forms.Padding(0, 0, 0, 15);
+            this.SignAnalystButton.Size = new System.Drawing.Size(154, 156);
+            this.SignAnalystButton.TabIndex = 3;
+            this.SignAnalystButton.Text = "Аналитик";
+            this.SignAnalystButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.SignAnalystButton.UseVisualStyleBackColor = true;
+            this.SignAnalystButton.Click += new System.EventHandler(this.SignAnalystButton_Click);
+            // 
+            // SignInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SignAnalystButton);
+            this.Controls.Add(this.SingExpertButton);
+            this.Controls.Add(this.SignInAsLabel);
+            this.Controls.Add(this.TitleLabel);
+            this.Name = "SignInForm";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label SignInAsLabel;
+        private System.Windows.Forms.Button SingExpertButton;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Button SignAnalystButton;
+    }
+}
+
---
+++ SystemAnalysis1/SignInForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemAnalysis1
+{
+    public partial class SignInForm : Form
+    {
+        public SignInForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void SingExpertButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            ExpertSigninForm expertSigninForm = new ExpertSigninForm();
+            expertSigninForm.Closed += (s, args) => Show();
+            expertSigninForm.Show();
+        }
+        private void SignAnalystButton_Click(object sender, EventArgs e)
+        {
+            AnalystForm analystForm = new AnalystForm();
+            analystForm.Show();
+        }
+    }
+}
